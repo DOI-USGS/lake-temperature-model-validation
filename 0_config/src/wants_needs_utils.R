79daa56c1afdc67fe8589c34412af3c68c1bc4d3
Oliver <soliver@usgs.gov>
2018-06-22 17:06:16 -0500

    added documentation for wants and needs functions.

--- 0_config/src/wants_needs_utils.R
+++ 0_config/src/wants_needs_utils.R
@@ -1,4 +1,11 @@
-# wants and needs helper functions
+#' Calculate your WQP "wants" using a vector of sites and variables to create a dataframe of 
+#' all site/variable combinations.
+#' 
+#' @param wqp_sites A vector of all site IDs from which you would like to have WQP data. 
+#' @param wqp_variables List of lists that contain parameters of interest (e.g., temperature) and 
+#' all corresponding synonyms available in NWIS (e.g., "Temperature" and "Temperature, water").
+#' @return A dataframe with columns "site" and "variable" that captures all possible combinations of
+#' sites and variables of interest.
 
 wqp_calc_wants <- function(wqp_sites, wqp_variables) {
   
@@ -8,7 +15,12 @@ wqp_calc_wants <- function(wqp_sites, wqp_variables) {
   return(df)
 }
 
-# wqp_wants - wqp_haves = wqp_needs
+#' Calculate your WQP "needs" by finding the difference between your "wants" and "haves".
+#' 
+#' @param wqp_wants A dataframe of all site/variable combinations of interest.
+#' @param wqp_haves An inventory of all site/variable combinations for which you already have data.
+#' @return A dataframe that includes site/variable combinations which reflects site/variable combinations
+#' in wqp_wants that are missing in wqp_haves.
 wqp_calc_needs <- function(wqp_wants, wqp_haves) {
   
   haves <- readRDS(wqp_haves)
